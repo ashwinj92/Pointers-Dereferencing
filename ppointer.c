e85fc62be0ea14f632fa450211466ae238d858d6
ashwin j <ashwinj92@gmail.com>
2013-09-27 07:07:20 -0700

    Pointer implementations in C
    Pointer related prorgams in c

---
+++ ppointer.c
@@ -0,0 +1,26 @@
+/*pointers try by ashwin...was bored ...
+so tried some basic pointer stuff has some pointer tweaks which tries diff methods to get an array into place..in the order seen below*/
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+ int a[6],*p;
+ int i;
+   a[5]=224;
+   p=&a; *p=1;
+   p++; *p=20;
+   p=&a[2]; *p=30;
+   p=a+3; *p=40;
+   p=a; *(p+4)=50;
+   *(a+5) = 226;
+for(i=0;i<6;i++)
+{
+printf("%d\t",a[i]);
+}
+printf("\n");
+  printf("pointer address is %d --->\n",a[5]);
+  return 0;
+}
+
+
